7f44c85c5313ec7f2022ef44695e91aa64288975
Tehillah Kangamba <tehillahkangamba@gmail.com>
2021-04-17 14:54:23 -0500

    defined Note object

--- app/src/main/java/notepad/app/objects/Note.java
+++ app/src/main/java/notepad/app/objects/Note.java
@@ -1,4 +1,15 @@
 package notepad.app.objects;
 
+import java.util.Date;
+
 public class Note{
+    private String title;
+    private String userNote;
+    private Date createdDate;
+    private Date modifiedDate;
+    public Note()
+    {
+
+    }
+
 }
