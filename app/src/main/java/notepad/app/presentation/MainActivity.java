20570371cca1c210ee653286d95f254caf0dac08
Tehillah Kangamba <tehillahkangamba@gmail.com>
2021-04-17 19:12:26 -0500

    addd on click methods

--- app/src/main/java/notepad/app/presentation/MainActivity.java
+++ app/src/main/java/notepad/app/presentation/MainActivity.java
@@ -1,16 +1,34 @@
 package notepad.app.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 
 import notepad.app.R;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton logInBtn;
+    AppCompatButton skipBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logInBtn=findViewById(R.id.loginBtn);
+        skipBtn=findViewById(R.id.skipBtn);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login button stuff
+            }
+        });
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
